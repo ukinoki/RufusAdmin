a37c4b8ebe09d1ae00f3c095952dc2204bfd266b
ukinoki <serge.laine2@sfr.fr>
2020-01-15 04:20:23 +0100

    15/01/2019

--- rufusadmin.cpp
+++ rufusadmin.cpp
@@ -24,7 +24,7 @@ RufusAdmin::RufusAdmin(QWidget *parent) : QMainWindow(parent), ui(new Ui::RufusA
     Datas::I();
     // la version du programme correspond à la date de publication, suivie de "/" puis d'un sous-n° - p.e. "23-6-2017/3"
     qApp->setApplicationName("RufusAdmin");
-    qApp->setApplicationVersion("14-01-2020/1");       // doit impérativement être composé de date version / n°version);
+    qApp->setApplicationVersion("15-01-2020/1");       // doit impérativement être composé de date version / n°version);
 
     ui->setupUi(this);
     setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
@@ -3044,9 +3044,9 @@ void RufusAdmin::DefinitScriptBackup(QString pathdirdestination, bool AvecImages
     scriptbackup += "RUFUSADMININI=\"" + QDir::homePath() + FILE_INI + "\"";
     //# Identifiants MySQL
     scriptbackup += "\n";
-    scriptbackup += "MYSQL_USER=\"" NOM_DUMPUSER "\"";
+    scriptbackup += "MYSQL_USER=\"" LOGIN_SQL "\"";
     scriptbackup += "\n";
-    scriptbackup += "MYSQL_PASSWORD=\"" MDP_DUMPUSER "\"";
+    scriptbackup += "MYSQL_PASSWORD=\"" MDP_SQL "\"";
     //# Commandes MySQL
     QDir Dir(QCoreApplication::applicationDirPath());
     Dir.cdUp();
