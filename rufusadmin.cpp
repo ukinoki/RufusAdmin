6a9bd404f57fb08b78b74bb26d813b1d534a3289
ukinoki <serge.laine2@sfr.fr>
2020-03-16 07:16:43 +0100

    16/03/2020

--- rufusadmin.cpp
+++ rufusadmin.cpp
@@ -24,7 +24,7 @@ RufusAdmin::RufusAdmin(QWidget *parent) : QMainWindow(parent), ui(new Ui::RufusA
     Datas::I();
     // la version du programme correspond à la date de publication, suivie de "/" puis d'un sous-n° - p.e. "23-6-2017/3"
     qApp->setApplicationName("RufusAdmin");
-    qApp->setApplicationVersion("15-03-2020/1");       // doit impérativement être composé de date version / n°version);
+    qApp->setApplicationVersion("16-03-2020/1");       // doit impérativement être composé de date version / n°version);
 
     ui->setupUi(this);
     setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
