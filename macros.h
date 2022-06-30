6481086f9804dc0f778651395ba761bca3813609
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2022-06-30 08:37:48 +0200

    30/06/2022

--- macros.h
+++ macros.h
@@ -1029,4 +1029,6 @@ along with RufusAdmin and Rufus.  If not, see <http://www.gnu.org/licenses/>.
 #define Param_Poste_Tono                                "Param_Poste/Tonometre"
 #define Param_Poste_Refracteur                          "Param_Poste/Refracteur"
 
+#define Dossier_ClesSSL                                 "/DossierClesSSL"
+
 #endif // MACROS_H
